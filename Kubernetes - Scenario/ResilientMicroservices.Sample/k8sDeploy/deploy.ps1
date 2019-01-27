a593a2ba21be693fceefe6e41abb01768e3f064f
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2019-01-27 18:45:06 +0100

    added configmaps and secrets

--- Kubernetes - Scenario/ResilientMicroservices.Sample/k8sDeploy/deploy.ps1
+++ Kubernetes - Scenario/ResilientMicroservices.Sample/k8sDeploy/deploy.ps1
@@ -3,37 +3,42 @@ $dockerRegistry = "localhost:5000"
 $releaseVersion = "0.1.0"
 $ErrorActionPreference = "Stop"
 
-Write-Host "Building docker images" -ForegroundColor Yellow
-docker build -t $dockerRegistry/customerservice:$releaseVersion ..\ -f ..\Customers.Web\DockerFile
-docker build -t $dockerRegistry/orderservice:$releaseVersion ..\ -f ..\Orders.Web\DockerFile
-if ($LASTEXITCODE -ne 0) {
-    throw "Failed to build docker images."
-} else {
-    Write-Host "Successfully built docker images." -ForegroundColor Green
-}
+# Write-Host "Building docker images" -ForegroundColor Yellow
+# docker build -t $dockerRegistry/customerservice:$releaseVersion ..\ -f ..\Customers.Web\DockerFile
+# docker build -t $dockerRegistry/orderservice:$releaseVersion ..\ -f ..\Orders.Web\DockerFile
+# if ($LASTEXITCODE -ne 0) {
+#     throw "Failed to build docker images."
+# } else {
+#     Write-Host "Successfully built docker images." -ForegroundColor Green
+# }
 
-Write-Host "Publishing docker images" -ForegroundColor Yellow
-docker push $dockerRegistry/customerservice:$releaseVersion
-docker push $dockerRegistry/orderservice:$releaseVersion
-if ($LASTEXITCODE -ne 0) {
-    throw "Failed to publish docker images."
-} else {
-    Write-Host "Successfully published docker images." -ForegroundColor Green
-}
+# Write-Host "Publishing docker images" -ForegroundColor Yellow
+# docker push $dockerRegistry/customerservice:$releaseVersion
+# docker push $dockerRegistry/orderservice:$releaseVersion
+# if ($LASTEXITCODE -ne 0) {
+#     throw "Failed to publish docker images."
+# } else {
+#     Write-Host "Successfully published docker images." -ForegroundColor Green
+# }
 
-Write-Host "Creating namespace in k8s" -ForegroundColor Yellow
-& kubectl  create namespace $namespace 
-& kubectl  get namespace $namespace
-if ($LASTEXITCODE -ne 0) {
-    throw "$namespace cannot be created."
-} else {
-    Write-Host "Successfully created namespace $namespace" -ForegroundColor Green
-}
+# Write-Host "Creating namespace in k8s" -ForegroundColor Yellow
+# & kubectl  create namespace $namespace 
+# & kubectl  get namespace $namespace
+# if ($LASTEXITCODE -ne 0) {
+#     throw "$namespace cannot be created."
+# } else {
+#     Write-Host "Successfully created namespace $namespace" -ForegroundColor Green
+# }
 
-Write-Host "Setup k8s requirements" -ForegroundColor Yellow
-& kubectl config set-context $(& kubectl  config current-context) --namespace=$namespace
+# Write-Host "Setup k8s requirements" -ForegroundColor Yellow
+# & kubectl config set-context $(& kubectl  config current-context) --namespace=$namespace
 
 Write-Host "Starting k8s deployment" -ForegroundColor Yellow
+& kubectl apply -f "$PSScriptRoot\k8s\configuration"
+Push-Location "$PSScriptRoot\k8s\configuration"
+& kubectl create secret generic customerservicesecret --from-file="customerservice.settings.json" --validate=false
+& kubectl create secret generic orderservicesecret --from-file="orderservice.settings.json" --validate=false
+Pop-Location
 & kubectl apply -f "$PSScriptRoot\k8s"
 If ($LASTEXITCODE -ne 0) {
     throw "Failed to create deployments in k8s"
